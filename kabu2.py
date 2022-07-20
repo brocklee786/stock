43a1b57bc9bc4aaeb15d1d2e8ff78d6827203dcf
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2022-07-20 11:28:49 +0900

    Update kabu2.py

--- kabu2.py
+++ kabu2.py
@@ -27,7 +27,7 @@ def get_basic_info(option):
         if th is None:
             continue
        
-        td = li.find('td')
+        td = td.find('td')
    
         key = th.text
         value = td.text
