d3effe7b5357889cc7babd6a2b74426ac9506203
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2022-09-21 13:41:09 +0900

    Update 01_株価評価.py

--- 01_株価評価.py
+++ 01_株価評価.py
@@ -44,11 +44,11 @@ from ctypes import *
 lib = CDLL("/home/appuser/lib/libta_lib.so.0.0.0")
 # import library
 try:
-    import talib as ta
+    import ta-lib as ta
 except ImportError:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "--global-option=build_ext", "--global-option=-L/home/appuser/lib/", "--global-option=-I/home/appuser/include/", "ta-lib"])
 finally:
-    import talib as ta
+    import ta-lib as ta
 
 
 st.set_page_config(layout="wide")
