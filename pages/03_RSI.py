88f1ff40e1e1aae8131161f490895605abc16760
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2022-10-15 13:09:30 +0900

    Update 03_RSI.py

--- pages/03_RSI.py
+++ pages/03_RSI.py
@@ -194,6 +194,22 @@ if option:
         probability2 = len(change2) * 100 / (len(change2) + len(no_change2))
         probability2 = int(probability2)
 
+        percent50_raise = []
+        percent50_unraise = []
+
+        #50%を超えたときに上昇が続いているか(順張り)
+        for i in range(15,days):
+                raise_continue = source['sma01'][2000-i+4] - source['sma01'][2000-i-1]
+                if source['RSI'][2000-i-1] > 50 and source['RSI'][2000-i-2] < 50:
+                        if raise_continue > 0:
+                                percent50_raise.append(1)
+                        else:
+                                percent50_unraise.append(1)
+
+        percent50_probability = len(percent50_raise) * 100 / (len(percent50_raise) + len(percent50_unraise))
+        percent50_probability = int(percent50_probability)
+        
+        st.write('RSI50%を超えた時、上昇トレンドが続く確率は'+str(percent50_probability) + '%')
         
         
 
@@ -207,3 +223,51 @@ if option:
                 st.image(image2,use_column_width='TRUE')
                 st.write('下降トレンドから上昇トレンドへの転換時の平均RSIは'+str(AVEG_RSI2) + '%')
                 st.write('確率は' + str(probability2) + '%')
+                
+　　　　change3_probability = []
+        #フィッティングした際にRSIが一番高くなるものを算出
+        for i in range(1,20):
+                #RSI
+                # 前日との差分を計算
+                source = source.loc[::-1]
+                
+                df_diff = source["Close"].diff(1)
+
+                # 計算用のDataFrameを定義
+                df_up, df_down = df_diff.copy(), df_diff.copy()
+
+                # df_upはマイナス値を0に変換
+                # df_downはプラス値を0に変換して正負反転
+                df_up[df_up < 0] = 0
+                df_down[df_down > 0] = 0
+                df_down = df_down * -1
+
+                # 期間14でそれぞれの平均を算出
+                df_up_sma_random = df_up.rolling(window=i, center=False).mean()
+                df_down_sma_random = df_down.rolling(window=i, center=False).mean()
+                source["RSI"] = 100.0 * (df_up_sma_random / (df_up_sma_random + df_down_sma_random))
+                source = source.loc[::-1]
+                change3 =  []
+                no_change3 = []
+                for k in range(15,days):
+                        #下降トレンドから上昇トレンドのとき
+                        
+                        sub1 = source['RSI'][2000-k] - source['RSI'][2000-k-1]
+                        sub2 = source['RSI'][2000-k-1] - source['RSI'][2000-k-2]
+                        sub3 = source['RSI'][2000-k-1+14]
+                        if sub1>0 and sub2<0 and source['RSI'][2000-k-3] < 40:
+                                for a in range(1,5):
+                                        trend_change = source['sma01'][2000-k+a] - source['sma01'][2000-k-1+a]
+                                        if trend_change > 0:
+                                                change3.append(1)
+                                                break
+                                        else:
+                                                no_change3.append(1)
+
+                probability3 = len(change3) * 100 / (len(change3) + len(no_change3))
+                probability3 = int(probability3)
+                change3_probability.append(probability3)
+
+        max = max(change3_probability)
+        max_day = change3_probability.index(max)
+        st.write('RSIが' + str(max_day) + '日間のとき、確率が最大で' + str(max) + '%です')
