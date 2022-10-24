b191326c7498f5d33543f7d8ef1f12e5f9e4ba8e
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2022-10-24 12:33:35 +0900

    Update 03_RSI.py

--- pages/03_RSI.py
+++ pages/03_RSI.py
@@ -216,12 +216,12 @@ if option:
 
         with left_column:
                 st.write('<span style="color:green">上昇トレンドから下降トレンドへの転換時</span>', unsafe_allow_html=True)
-                st.image(image,use_column_width='FALSE')
+                st.image(image,use_column_width='auto')
                 st.write('上昇トレンドから下降トレンドへの転換時の平均RSIは'+str(AVEG_RSI) + '%')
                 st.write('確率は' + str(probability) + '%')
         with right_column:
                 st.write('<span style="color:green">下降トレンドから上昇トレンドへの転換時</span>', unsafe_allow_html=True)
-                st.image(image,use_column_width='FALSE')
+                st.image(image,use_column_width='auto')
                 st.write('下降トレンドから上昇トレンドへの転換時の平均RSIは'+str(AVEG_RSI2) + '%')
                 st.write('確率は' + str(probability2) + '%')
         
