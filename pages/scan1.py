effce9f07f46cfd7c781e77c82a9abef201cd7c6
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2022-11-24 21:21:29 +0900

    Update scan1.py

--- pages/scan1.py
+++ pages/scan1.py
@@ -337,7 +337,7 @@ for code in codes:
         price_change = price_days - price_buy
         adx_direction = source['ADX'][i] - source['ADX'][i-1]
 
-        if slow_percentd_yesterday<20 and slow_percentd>20 and percentk_direction>0 and percentk>70:
+        if slow_percentd_yesterday<20 and slow_percentd>20 and percentk_direction>0 and percentk>60:
             check3_all.append(i)
             
 
