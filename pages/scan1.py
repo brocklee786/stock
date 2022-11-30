fa5acbfcaad07febb3cda6aacba19b53b1bcddf9
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2022-11-30 13:08:35 +0900

    Update scan1.py

--- pages/scan1.py
+++ pages/scan1.py
@@ -55,7 +55,7 @@ if st.button('計算を行う'):
       option = code
       ticker = str(option) + '.T'
       tkr = yf.Ticker(ticker)
-      hist = tkr.history(period='300d')
+      hist = tkr.history(period='600d')
       hist = hist.reset_index()
       hist = hist.set_index(['Date'])
       hist = hist.rename_axis('Date').reset_index()
@@ -223,7 +223,7 @@ if st.button('計算を行う'):
       price1_win = []
       st.write(code)
 
-      for i in range(60,285):
+      for i in range(60,585):
           conversion_line = source['conversion_line'][i]
           conversion_line_yesterday = source['conversion_line'][i-1]
           base_line = source['base_line'][i]
@@ -280,7 +280,7 @@ if st.button('計算を行う'):
        st.write(code,1,len(check1_down))
 
       #大循環MACD
-      for i in range(60,285):
+      for i in range(60,585):
           macd1 = source['MACD1'][i]
           macd1_direction = source['MACD1'][i] - source['MACD1'][i-3]
           macd2 = source['MACD2'][i]
@@ -330,7 +330,7 @@ if st.button('計算を行う'):
       price_dif3 = []
       price3_win = []
 
-      for i in range(60,285):
+      for i in range(60,585):
           percentk = source['sct_k_price'][i]
           percentk_direction = source['sct_k_price'][i] - source['sct_k_price'][i-1]
           slow_percentd = source['slow_sct_d_price'][i]
@@ -379,7 +379,7 @@ if st.button('計算を行う'):
       price_dif4 = []
       price4_win = []
       
-      for i in range(70,280):
+      for i in range(70,580):
           price = source['Close'][i]
           price_buy = source['Close'][i+1]
           price_days = source['Close'][i+days+1]
