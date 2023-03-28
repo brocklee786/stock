cdf2c148e0b584edaa272f9481abf2bb561c1fca
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2023-03-28 12:16:36 +0900

    Update 08_テクニカルレポート.py

--- pages/08_テクニカルレポート.py
+++ pages/08_テクニカルレポート.py
@@ -17,50 +17,23 @@ import subprocess
 
 
 app_id = '47d4ff02a160e5f085f1baf6ff2dea46e8110488'
-stats_data_id = '0003109785' # 年次別にみた人口動態総覧
+stats_data_id = '0003109785' # 年次別にみた人口動態総覧GDP
 
-# メタ情報取得のURL
-url = 'https://api.e-stat.go.jp/rest/3.0/app/json/getMetaInfo?'
+# 統計データ取得のURL
+url = 'https://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?'
 url += 'appId={0:s}&'.format(app_id) 
 url += 'statsDataId={0:s}&'.format(stats_data_id)
-url += 'explanationGetFlg=N&'   # 解説情報有無：無し
+url += 'metaGetFlg=N&'          # メタ情報有無
+url += 'explanationGetFlg=N&'   # 解説情報有無
+url += 'annotationGetFlg=N&'    # 注釈情報有無
 
-# メタ情報取得
-meta_info = requests.get(url).json()
+# 統計データ取得
+json = requests.get(url).json()
 
-# 統計データのカテゴリ要素をID(数字の羅列)から、意味がわかる名称に変更する
-for class_obj in meta_info:
+# 統計データからデータ部取得
+values = json['GET_STATS_DATA']['STATISTICAL_DATA']['DATA_INF']['VALUE']
 
-    # メタ情報の「@id」の先頭に'@'を付与'した文字列が、
-    # 統計データの列名と対応している
-    column_name = '@' + class_obj['@id']
-    
-    # 統計データの列名を「@code」から「@name」に置換するディクショナリを作成
-    id_to_name_dict = {}
-    for obj in class_obj['CLASS']:
-        id_to_name_dict[obj['@code']] = obj['@name']
-    
-    # ディクショナリを用いて、指定した列の要素を置換 
-    df[column_name] = df[column_name].replace(id_to_name_dict)
-
-# 統計データの列名を変換するためのディクショナリを作成
-col_replace_dict = {'@unit': '単位', '$': '値'}
-for class_obj in meta_info:
-    org_col = '@' + class_obj['@id']
-    new_col = class_obj['@name']
-    col_replace_dict[org_col] = new_col
-
-# ディクショナリに従って、列名を置換する
-new_columns = []
-for col in stats_data:
-    if col in col_replace_dict:
-        new_columns.append(col_replace_dict[col])
-    else:
-        new_columns.append(col)
-        
-df.columns = new_columns
-
+# jsonからDataFrameを作成
+df = pd.DataFrame(values)
 st.write(df)
-
-
 
