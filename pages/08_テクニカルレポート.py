861574c9e7af499c9c8dda1d256295ce39598804
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2023-03-28 11:39:09 +0900

    Update and rename 08_逆張り.py to 08_テクニカルレポート.py

---
+++ pages/08_テクニカルレポート.py
@@ -0,0 +1,42 @@
+from math import nan
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+import re
+import matplotlib
+import matplotlib.pyplot as plt
+import yfinance as yf
+import altair as alt
+import os
+import sys
+import subprocess
+
+
+
+app_id = '47d4ff02a160e5f085f1baf6ff2dea46e8110488'
+stats_data_id = '0003427113' # 年次別にみた人口動態総覧
+
+# 統計データ取得のURL
+url = 'https://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?'
+url += 'appId={0:s}&'.format(app_id) 
+url += 'statsDataId={0:s}&'.format(stats_data_id)
+url += 'metaGetFlg=N&'          # メタ情報有無
+url += 'explanationGetFlg=N&'   # 解説情報有無
+url += 'annotationGetFlg=N&'    # 注釈情報有無
+
+# 統計データ取得
+json = requests.get(url).json()
+
+# 統計データからデータ部取得
+values = json['GET_STATS_DATA']['STATISTICAL_DATA']['DATA_INF']['VALUE']
+
+# jsonからDataFrameを作成
+df = pd.DataFrame(values)
+st.write(df)
+
+
+
+
