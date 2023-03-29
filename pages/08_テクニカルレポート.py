babb35c5a537fd29ee30fdf3b0abf45db8492306
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2023-03-30 07:57:00 +0900

    Update 08_テクニカルレポート.py

--- pages/08_テクニカルレポート.py
+++ pages/08_テクニカルレポート.py
@@ -89,7 +89,7 @@ values = json['GET_STATS_DATA']['STATISTICAL_DATA']['DATA_INF']['VALUE']
 
 # jsonからDataFrameを作成
 df = pd.DataFrame(values)
-df3=['$']
+df3=[]
 df3=pd.DataFrame(df3)
 for data in range(5000):
   if df['@tab'][data] == '100' and df['@cat01'][data] =='100':
