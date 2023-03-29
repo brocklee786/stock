0f121cf57f517919124be3cc944d9f917b590d16
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2023-03-30 07:54:21 +0900

    Update 08_テクニカルレポート.py

--- pages/08_テクニカルレポート.py
+++ pages/08_テクニカルレポート.py
@@ -93,7 +93,7 @@ df3=['$']
 df3=pd.DataFrame(df3)
 for data in range(5000):
   if df['@tab'][data] == '100' and df['@cat01'][data] =='100':
-    df3['$'].append(df['$'][data])
+    df3.append(df['$'][data])
     
 st.write(df3)
     
