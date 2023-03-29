6056feb94e53cfa517db009c4032ab2aaa418710
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2023-03-29 12:47:53 +0900

    Update 08_テクニカルレポート.py

--- pages/08_テクニカルレポート.py
+++ pages/08_テクニカルレポート.py
@@ -65,3 +65,56 @@ plt.gcf().autofmt_xdate()      # X軸値を45度回転
 plt.savefig("date_graph3.jpg") # 画像保存
 image = Image.open('date_graph3.jpg')
 st.image(image, caption='GDP [10億円]',width=600)
+
+
+
+
+
+#景気動向指数
+stats_data_id = '0003446461' # CI
+
+# 統計データ取得のURL
+url = 'https://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?'
+url += 'appId={0:s}&'.format(app_id) 
+url += 'statsDataId={0:s}&'.format(stats_data_id)
+url += 'metaGetFlg=N&'          # メタ情報有無
+url += 'explanationGetFlg=N&'   # 解説情報有無
+url += 'annotationGetFlg=N&'    # 注釈情報有無
+
+# 統計データ取得
+json = requests.get(url).json()
+
+# 統計データからデータ部取得
+values = json['GET_STATS_DATA']['STATISTICAL_DATA']['DATA_INF']['VALUE']
+
+# jsonからDataFrameを作成
+df = pd.DataFrame(values)
+
+
+df2 = df.tail(10)
+df2 = df2.reset_index(drop=True)
+df2['date'] = 1
+
+for i in range(10):
+  date = df2['@time'][i] 
+  date = date[:4] + date[6:]
+  
+  date_object = datetime.datetime.strptime(date, "%Y%m%d")
+  formatted_date = date_object.strftime("%Y-%m-%d")
+  df2['date'][i] = formatted_date
+
+st.write(df2)
+# #GDPの値を文字列から数値に変換
+# df2['$'] = df2['$'].astype('float')
+
+# X = df2['date']
+# Y = df2['$']
+# # グラフ可視化（折れ線グラフ）
+# plt.plot_date(X, Y, label='GDP', linestyle='solid')
+
+# # 書式設定
+# plt.legend(loc="best")         # 凡例
+# plt.gcf().autofmt_xdate()      # X軸値を45度回転
+# plt.savefig("date_graph3.jpg") # 画像保存
+# image = Image.open('date_graph3.jpg')
+# st.image(image, caption='GDP [10億円]',width=600)
