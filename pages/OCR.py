95be86ee8fa7bcbd921dcdf9cccb4ab7b553b691
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2023-04-30 12:46:47 +0900

    Update OCR.py

--- pages/OCR.py
+++ pages/OCR.py
@@ -1,5 +1,18 @@
 import streamlit as st
-
+import pytesseract
+from PIL import Image
 
 
 my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+if my_upload:
+    img = fix_image("./ocr.jpg")
+    
+# OCRエンジンのパスを設定
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# 画像から文字を読み取る
+text = pytesseract.image_to_string(img, lang='jpn')
+
+# 結果の表示
+st.write(text)
