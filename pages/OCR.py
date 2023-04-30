a24aeb603275720f744175685118d47df22e19be
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2023-04-30 13:32:35 +0900

    Update OCR.py

--- pages/OCR.py
+++ pages/OCR.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw
 import easyocr
 import streamlit as st
 
-reader = easyocr.Reader(['j','en'])
+reader = easyocr.Reader(['ja','en'])
 selected_image = st.file_uploader('upload image', type='jpeg',)
 
 original_image = st.empty()
