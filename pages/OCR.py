62cc721ccfb7d7767187e56197d76e3010bbf284
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2023-05-01 10:05:56 +0900

    OCR.py を更新

--- pages/OCR.py
+++ pages/OCR.py
@@ -4,7 +4,7 @@ import easyocr
 import streamlit as st
 
 reader = easyocr.Reader(['ja','en'])
-selected_image = st.file_uploader('upload image', type='jpeg',)
+selected_image = st.file_uploader('upload image', type='jpg',)
 
 original_image = st.empty()
 result_image = st.empty()
