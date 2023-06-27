1a242f613be86e05d814aae63e7174507eb85f70
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2023-06-28 08:05:46 +0900

    Update 移動平均線大循環分析.py

--- pages/移動平均線大循環分析.py
+++ pages/移動平均線大循環分析.py
@@ -19,6 +19,9 @@ import seaborn
 from scipy.stats import linregress
 
 st.set_page_config(layout="wide")
+codes1 = ['5901','6810','6804','6779','6770','6754','6753','6752','6750','6724','6723','5727','5802','5805','5929','5943','6013','6055','6058','6062','6070','6101','6113','6141','6208','6250','6269','6287','6298','6338','6395','6407','6463','6513','6630','6666','6871','6925','6937','6941','6952','6962','6995','6999','7202','7261','7270','7296','7313','7254','7414','7421','7453','7532','7545','7575','7606','7613','7616','7718','7730','7732','7731','7752','7760','7867','7906','7915','7965','7970','7981','7984','7994','8020','8086','8129','8130','8133','8174','8233','8253','8282','8341','8411','8699','8795','8802','8804','8920','8923','8929','8934','9005','9007','9076','9308','9401','9409','9503','9511','9513','9625','9692','9832','1712','1808','1826','1944','1969','2002','2154','2158','2160','2301','2309','2389','2395','2427','2432','2433','2438','2471','2503','2531','2579','2607','2613','2678','2681','2685','2730','2784','2792','2910','2929','3003','3048','3076','3086','3099','3105','3107','3161','3254','3319','3377','3405','3407','3433','3436','3591','3604','3632','3657','3661','3675','4028','4042','4045','4080','4095','4204','4331']
+
+st.title("移動平均線大循環分析")
 days = 100
 option = st.text_input('銘柄コードを入力してください')
 if option:
@@ -63,7 +66,6 @@ if option:
     stage6_list = []
     stage4_list = []
     stage5_list = []
-
     for a in range(last+1):
         #ステージの決定
         if source['sma01'][a]>source['sma02'][a]>source['sma03'][a]:
@@ -82,11 +84,26 @@ if option:
             source["stage"][a] = 6
             stage6_list.append(a)
 
+    
+    stage_now = source['stage'][last]
+    #st.table(source)
+    
 
 
-    stage6_end = stage6_list[-1]
-    stage4_end = stage4_list[-1]
-    stage5_end = stage5_list[-1]
+    for num in range(last+1):
+        index_num = last - num
+        if stage_now == source['stage'][index_num]:
+            continue
+        if stage_now != source['stage'][index_num]:
+            changed_when = index_num
+            break
+
+    #st.write(changed_when)
+
+        
+
+
+
         
 
     stage1_now = []
@@ -94,9 +111,8 @@ if option:
     stage6_now = []
     #ステージ１
     if source['sma01'][last]>source['sma02'][last]>source['sma03'][last]:
-        stage="第1ステージ"
-        st.write("第1ステージ")
-        for a in range(stage6_end+1, last+1):
+        st.subheader("現在のステージ:第1ステージ")
+        for a in range(changed_when+1, last+1):
             stage1_now.append(a)
 
         sma02_sma01 = []
@@ -119,9 +135,13 @@ if option:
         b = model.intercept_
         #st.write('a = ', model.coef_) 
         #st.write('b = ', model.intercept_)
-        st.pyplot(figure)
+        #st.pyplot(figure)
         days_until6 = -b/a - last
-        st.write('第2ステージまで',int(days_until6))
+        if a > 0:
+            st.subheader('上昇トレンド継続中')
+        else:
+            st.subheader('予想では第2ステージまで'+ str(int(days_until6)) + '日間')
+            
         #短期の傾き
         tilt_short = source['sma01'][last] - source['sma01'][last-1]
         tilt_short2 = (source['sma01'][last] - source['sma01'][last-3]) / 3
@@ -131,26 +151,33 @@ if option:
         #長期の傾き
         tilt_long = source['sma03'][last] - source['sma03'][last-1]
         tilt_long2 = (source['sma03'][last] - source['sma03'][last-3]) / 3
-        st.write("短期の傾き", tilt_short, tilt_short2)
-        st.write("中期の傾き", tilt_mid, tilt_mid2)
-        st.write("長期の傾き", tilt_long, tilt_long2)
+        st.subheader('移動平均線の傾き')
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.subheader("短期の傾き")
+            st.subheader("1日:" + str(tilt_short))
+            st.subheader("3日:" + str(tilt_short2))
+        with col2:
+            st.subheader("中期の傾き")
+            st.subheader("1日:" + str(tilt_mid))
+            st.subheader("3日:" + str(tilt_mid2))
+        with col3:
+            st.subheader("長期の傾き")
+            st.subheader("1日:" + str(tilt_long))
+            st.subheader("3日:" + str(tilt_long2))
     #ステージ２
     if source['sma02'][last]>source['sma01'][last]>source['sma03'][last]:
-        stage="第2ステージ"
-        st.write("第2ステージ")
+        st.subheader("第2ステージ")
     #ステージ３
     if source['sma02'][last]>source['sma03'][last]>source['sma01'][last]:
-        stage="第3ステージ"
-        st.write("第3ステージ")
+        st.subheader("第3ステージ")
     #ステージ４
     if source['sma03'][last]>source['sma02'][last]>source['sma01'][last]:
-        stage="第4ステージ"
-        st.write("第4ステージ")
+        st.subheader("第4ステージ")
     #ステージ５
     if source['sma03'][last]>source['sma01'][last]>source['sma02'][last]:
-        stage="第5ステージ"
-        st.write("第5ステージ")
-        for a in range(stage4_end+1, last+1):
+        st.subheader("第5ステージ")
+        for a in range(changed_when+1, last+1):
             stage5_now.append(a)
 
         sma03_sma01 = []
@@ -173,9 +200,12 @@ if option:
         b = model.intercept_
         #st.write('a = ', model.coef_) 
         #st.write('b = ', model.intercept_)
-        st.pyplot(figure)
+        #st.pyplot(figure)
         days_until6 = -b/a - last
-        st.write('第6ステージまで',int(days_until6))
+        if a < 0:
+            st.subheader('第6ステージまで',int(days_until6))
+        else:
+            st.subheader('逆行の可能性')
         #短期の傾き
         tilt_short = source['sma01'][last] - source['sma01'][last-1]
         tilt_short2 = (source['sma01'][last] - source['sma01'][last-3]) / 3
@@ -185,16 +215,25 @@ if option:
         #長期の傾き
         tilt_long = source['sma03'][last] - source['sma03'][last-1]
         tilt_long2 = (source['sma03'][last] - source['sma03'][last-3]) / 3
-        st.write("短期の傾き", tilt_short, tilt_short2)
-        st.write("中期の傾き", tilt_mid, tilt_mid2)
-        st.write("長期の傾き", tilt_long, tilt_long2)
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.subheader("短期の傾き")
+            st.subheader("1日:" + str(tilt_short))
+            st.subheader("3日:" + str(tilt_short2))
+        with col2:
+            st.subheader("中期の傾き")
+            st.subheader("1日:" + str(tilt_mid))
+            st.subheader("3日:" + str(tilt_mid2))
+        with col3:
+            st.subheader("長期の傾き")
+            st.subheader("1日:" + str(tilt_long))
+            st.subheader("3日:" + str(tilt_long2))
 
 
     #ステージ６
     if source['sma01'][last]>source['sma03'][last]>source['sma02'][last]:
-        stage="第6ステージ"
-        st.write("第6ステージ")
-        for a in range(stage5_end+1, last+1):
+        st.subheader("第6ステージ")
+        for a in range(changed_when+1, last+1):
             stage6_now.append(a)
 
         sma03_sma02 = []
@@ -217,9 +256,11 @@ if option:
         b = model.intercept_
         #st.write('a = ', model.coef_) 
         #st.write('b = ', model.intercept_)
-        st.pyplot(figure)
-        days_until1 = -b/a - last
-        st.write('第1ステージまで',int(days_until1))
+        #st.pyplot(figure)
+        if a < 0:
+            st.subheader('第1ステージまで',int(days_until6))
+        else:
+            st.subheader('逆行の可能性')
         #短期の傾き
         tilt_short = source['sma01'][last] - source['sma01'][last-1]
         tilt_short2 = (source['sma01'][last] - source['sma01'][last-3]) / 3
@@ -229,105 +270,224 @@ if option:
         #長期の傾き
         tilt_long = source['sma03'][last] - source['sma03'][last-1]
         tilt_long2 = (source['sma03'][last] - source['sma03'][last-3]) / 3
-        st.write("短期の傾き", tilt_short, tilt_short2)
-        st.write("中期の傾き", tilt_mid, tilt_mid2)
-        st.write("長期の傾き", tilt_long, tilt_long2)
-
-            ## チャートを単回帰分析し、得られる単回帰直線よりも上（下）の値だけで再度単回帰分析...
-        ## これを繰り返し、高値（安値）を2～3点に絞り込む
-
-    # 高値の始点/支点を取得
-    def get_highpoint(start, end):
-        chart = source[start:end+1]
-        while len(chart)>3:
-            regression = linregress(
-                x = chart['time_id'],
-                y = chart['High'],
-            )
-            chart = chart.loc[chart['High'] > regression[0] * chart['time_id'] + regression[1]]
-        return chart
-
-    # 安値の始点/支点を取得
-    def get_lowpoint(start, end):
-        chart = source[start:end+1]
-        while len(chart)>3:
-            regression = linregress(
-                x = chart['time_id'],
-                y = chart['Low'],
-            )
-            chart = chart.loc[chart['Low'] < regression[0] * chart['time_id'] + regression[1]]
-        return chart
-
-    def g_trendlines(span=20, min_interval=3):
-        trendlines = []
-
-        # 高値の下降トレンドラインを生成
-        for i in source.index[::10]:
-            highpoint = get_highpoint(i, i + span)
-            # ポイントが2箇所未満だとエラーになるので回避する
-            if len(highpoint) < 2:
-                continue
-            # 始点と支点が近過ぎたらトレンドラインとして引かない
-            if abs(highpoint.index[0] - highpoint.index[1]) < min_interval:
-                continue
-            regression = linregress(
-                x = highpoint['time_id'],
-                y = highpoint['High'],
-            )
-            print(regression[0] < 0.0, 'reg_high: ', regression[0], ', ', regression[1], )
-
-            # 下降してるときだけ
-            if regression[0] < 0.0:
-                trendlines.append(regression[0] * source['time_id'][i:i+span*2] + regression[1])
-
-        # 安値の上昇トレンドラインを生成
-        for i in source.index[::10]:
-            lowpoint   = get_lowpoint(i, i + span)
-            # ポイントが2箇所未満だとエラーになるので回避する
-            if len(lowpoint) < 2:
-                continue
-            # 始点と支点が近過ぎたらトレンドラインとして引かない
-            if abs(lowpoint.index[0] - lowpoint.index[1]) < min_interval:
-                continue
-            regression = linregress(
-                x = lowpoint['time_id'],
-                y = lowpoint['Low'],
-            )
-            print(regression[0] > 0.0, 'reg_low: ', regression[0], ', ', regression[1], )
-
-            # 上昇してるときだけ
-            if regression[0] > 0.0:
-                trendlines.append(regression[0] * source['time_id'][i:i+span*2] + regression[1])
-
-        return trendlines
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.subheader("短期の傾き")
+            st.subheader("1日:" + str(tilt_short))
+            st.subheader("3日:" + str(tilt_short2))
+        with col2:
+            st.subheader("中期の傾き")
+            st.subheader("1日:" + str(tilt_mid))
+            st.subheader("3日:" + str(tilt_mid2))
+        with col3:
+            st.subheader("長期の傾き")
+            st.subheader("1日:" + str(tilt_long))
+            st.subheader("3日:" + str(tilt_long2))
 
 
-    figure, axis1 = plt.subplots(figsize=(15,7))
+st.subheader('ステージを探す')
+col1, col2, col3 = st.columns(3)
+with col1:
+    if st.button('ステージ５を探す'):
+        for code in codes1:
+            ticker = str(code) + '.T'
+            tkr = yf.Ticker(ticker)
+            hist = tkr.history(period=f'{days}d')
+            hist = hist.reset_index()
+            hist = hist.set_index(['Date'])
+            hist = hist.rename_axis('Date').reset_index()
+            hist = hist.T
+            a = hist.to_dict()
 
-    # ローソク足
-    mpf.candlestick2_ohlc(
-        axis1,
-        opens  = source.Open.values,
-        highs  = source.High.values,
-        lows   = source.Low.values,
-        closes = source.Close.values,
-        width=0.6, colorup='#77d879', colordown='#db3f3f'
-    )
+            for items in a.values():
+                    time = items['Date']
+                    items['Date'] = time.strftime("%Y/%m/%d")
 
-    # トレンドラインたちを引く
-    for i, line in enumerate(g_trendlines()):
-        axis1.plot(line, label=i)
+            b = [x for x in a.values()]
 
-    # X軸の見た目を整える
-    xticks_number  = 12 # 12本刻みに目盛りを書く
-    xticks_index   = range(0, len(source), xticks_number)
+            source = pd.DataFrame(b)
+            source['time_id'] = source.index + 1
+            price = source['Close']
+
+            #st.table(source)
+
+            #移動平均
+            span01=5
+            span02=25
+            span03=50
+
+            source['sma01'] = price.rolling(window=span01).mean()
+            source['sma02'] = price.rolling(window=span02).mean()
+            source['sma03'] = price.rolling(window=span03).mean()
+            
+            
+
+            last=99
+            source['x'] = source["sma01"] - source["sma02"]
+            source['y'] = source["sma03"] - source["sma01"]
+            source['z'] = source["sma03"] - source["sma02"]
+            
+            source["stage"] = 1
+            stage6_list = []
+            stage4_list = []
+            stage5_list = []
+            for a in range(last+1):
+                #ステージの決定
+                if source['sma01'][a]>source['sma02'][a]>source['sma03'][a]:
+                    source["stage"][a] = 1
+                if source['sma02'][a]>source['sma01'][a]>source['sma03'][a]:
+                    source["stage"][a] = 2
+                if source['sma02'][a]>source['sma03'][a]>source['sma01'][a]:
+                    source["stage"][a] = 3
+                if source['sma03'][a]>source['sma02'][a]>source['sma01'][a]:
+                    source["stage"][a] = 4
+                    stage4_list.append(a)
+                if source['sma03'][a]>source['sma01'][a]>source['sma02'][a]:
+                    source["stage"][a] = 5
+                    stage5_list.append(a)
+                if source['sma01'][a]>source['sma03'][a]>source['sma02'][a]:
+                    source["stage"][a] = 6
+                    stage6_list.append(a)
+
+            
+            stage_now = source['stage'][last]
+            if stage_now == 5:
+                st.write(code)
 
 
-    # axis1を装飾 ( plt.sca(axis): set current axis )
-    plt.sca(axis1)
+with col2:
+    if st.button('ステージ6を探す'):
+        for code in codes1:
+            ticker = str(code) + '.T'
+            tkr = yf.Ticker(ticker)
+            hist = tkr.history(period=f'{days}d')
+            hist = hist.reset_index()
+            hist = hist.set_index(['Date'])
+            hist = hist.rename_axis('Date').reset_index()
+            hist = hist.T
+            a = hist.to_dict()
 
-    plt.legend()
-    st.pyplot(figure,use_container_width=True)
+            for items in a.values():
+                    time = items['Date']
+                    items['Date'] = time.strftime("%Y/%m/%d")
+
+            b = [x for x in a.values()]
+
+            source = pd.DataFrame(b)
+            source['time_id'] = source.index + 1
+            price = source['Close']
+
+            #st.table(source)
+
+            #移動平均
+            span01=5
+            span02=25
+            span03=50
+
+            source['sma01'] = price.rolling(window=span01).mean()
+            source['sma02'] = price.rolling(window=span02).mean()
+            source['sma03'] = price.rolling(window=span03).mean()
+            
+            
+
+            last=99
+            source['x'] = source["sma01"] - source["sma02"]
+            source['y'] = source["sma03"] - source["sma01"]
+            source['z'] = source["sma03"] - source["sma02"]
+            
+            source["stage"] = 1
+            stage6_list = []
+            stage4_list = []
+            stage5_list = []
+            for a in range(last+1):
+                #ステージの決定
+                if source['sma01'][a]>source['sma02'][a]>source['sma03'][a]:
+                    source["stage"][a] = 1
+                if source['sma02'][a]>source['sma01'][a]>source['sma03'][a]:
+                    source["stage"][a] = 2
+                if source['sma02'][a]>source['sma03'][a]>source['sma01'][a]:
+                    source["stage"][a] = 3
+                if source['sma03'][a]>source['sma02'][a]>source['sma01'][a]:
+                    source["stage"][a] = 4
+                    stage4_list.append(a)
+                if source['sma03'][a]>source['sma01'][a]>source['sma02'][a]:
+                    source["stage"][a] = 5
+                    stage5_list.append(a)
+                if source['sma01'][a]>source['sma03'][a]>source['sma02'][a]:
+                    source["stage"][a] = 6
+                    stage6_list.append(a)
+
+            
+            stage_now = source['stage'][last]
+            if stage_now == 6:
+                st.write(code)
+with col3:
+    if st.button('ステージ1を探す'):
+        for code in codes1:
+            ticker = str(code) + '.T'
+            tkr = yf.Ticker(ticker)
+            hist = tkr.history(period=f'{days}d')
+            hist = hist.reset_index()
+            hist = hist.set_index(['Date'])
+            hist = hist.rename_axis('Date').reset_index()
+            hist = hist.T
+            a = hist.to_dict()
+
+            for items in a.values():
+                    time = items['Date']
+                    items['Date'] = time.strftime("%Y/%m/%d")
+
+            b = [x for x in a.values()]
+
+            source = pd.DataFrame(b)
+            source['time_id'] = source.index + 1
+            price = source['Close']
+
+            #st.table(source)
+
+            #移動平均
+            span01=5
+            span02=25
+            span03=50
+
+            source['sma01'] = price.rolling(window=span01).mean()
+            source['sma02'] = price.rolling(window=span02).mean()
+            source['sma03'] = price.rolling(window=span03).mean()
+            
+            
+
+            last=99
+            source['x'] = source["sma01"] - source["sma02"]
+            source['y'] = source["sma03"] - source["sma01"]
+            source['z'] = source["sma03"] - source["sma02"]
+            
+            source["stage"] = 1
+            stage6_list = []
+            stage4_list = []
+            stage5_list = []
+            for a in range(last+1):
+                #ステージの決定
+                if source['sma01'][a]>source['sma02'][a]>source['sma03'][a]:
+                    source["stage"][a] = 1
+                if source['sma02'][a]>source['sma01'][a]>source['sma03'][a]:
+                    source["stage"][a] = 2
+                if source['sma02'][a]>source['sma03'][a]>source['sma01'][a]:
+                    source["stage"][a] = 3
+                if source['sma03'][a]>source['sma02'][a]>source['sma01'][a]:
+                    source["stage"][a] = 4
+                    stage4_list.append(a)
+                if source['sma03'][a]>source['sma01'][a]>source['sma02'][a]:
+                    source["stage"][a] = 5
+                    stage5_list.append(a)
+                if source['sma01'][a]>source['sma03'][a]>source['sma02'][a]:
+                    source["stage"][a] = 6
+                    stage6_list.append(a)
+
+            
+            stage_now = source['stage'][last]
+            if stage_now == 1:
+                st.write(code)
+
 
 
     
