700b8aa44adaf5f8dbabe34187265d7ba77fb5b9
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2023-07-01 10:41:12 +0900

    Update 移動平均線大循環分析.py

--- pages/移動平均線大循環分析.py
+++ pages/移動平均線大循環分析.py
@@ -22,7 +22,7 @@ st.set_page_config(layout="wide")
 codes1 = ['5901','6810','6804','6779','6770','6754','6753','6752','6750','6724','6723','5727','5802','5805','5929','5943','6013','6055','6058','6062','6070','6101','6113','6141','6208','6250','6269','6287','6298','6338','6395','6407','6463','6513','6630','6666','6871','6925','6937','6941','6952','6962','6995','6999','7202','7261','7270','7296','7313','7254','7414','7421','7453','7532','7545','7575','7606','7613','7616','7718','7730','7732','7731','7752','7760','7867','7906','7915','7965','7970','7981','7984','7994','8020','8086','8129','8130','8133','8174','8233','8253','8282','8341','8411','8699','8795','8802','8804','8920','8923','8929','8934','9005','9007','9076','9308','9401','9409','9503','9511','9513','9625','9692','9832','1712','1808','1826','1944','1969','2002','2154','2158','2160','2301','2309','2389','2395','2427','2432','2433','2438','2471','2503','2531','2579','2607','2613','2678','2681','2685','2730','2784','2792','2910','2929','3003','3048','3076','3086','3099','3105','3107','3161','3254','3319','3377','3405','3407','3433','3436','3591','3604','3632','3657','3661','3675','4028','4042','4045','4080','4095','4204','4331']
 
 st.title("移動平均線大循環分析")
-days = 100
+days = 1000
 option = st.text_input('銘柄コードを入力してください')
 if option:
     ticker = str(option) + '.T'
@@ -57,7 +57,7 @@ if option:
     
     
 
-    last=99
+    last=999
     source['x'] = source["sma01"] - source["sma02"]
     source['y'] = source["sma03"] - source["sma01"]
     source['z'] = source["sma03"] - source["sma02"]
@@ -112,6 +112,7 @@ if option:
     #ステージ１
     if source['sma01'][last]>source['sma02'][last]>source['sma03'][last]:
         st.subheader("現在のステージ:第1ステージ")
+        st.subheader(str(source['Date'][changed_when])+'から')
         for a in range(changed_when+1, last+1):
             stage1_now.append(a)
 
@@ -151,8 +152,18 @@ if option:
         #長期の傾き
         tilt_long = source['sma03'][last] - source['sma03'][last-1]
         tilt_long2 = (source['sma03'][last] - source['sma03'][last-3]) / 3
-        st.subheader('移動平均線の傾き')
         col1, col2, col3 = st.columns(3)
+        #帯の広さ
+        width = source['sma02'][last] - source['sma03'][last]
+        if last - changed_when > 3:
+            width2 = source['sma02'][changed_when+3] - source['sma03'][changed_when+3]
+        else:
+            width2 = source['sma02'][changed_when] - source['sma03'][changed_when]
+        width_change = width - width2
+        if width_change>0:
+            st.subheader('帯拡大中')
+        else:
+            st.subheader('帯縮小の可能性')
         with col1:
             st.subheader("短期の傾き")
             st.subheader("1日:" + str(tilt_short))
@@ -165,6 +176,7 @@ if option:
             st.subheader("長期の傾き")
             st.subheader("1日:" + str(tilt_long))
             st.subheader("3日:" + str(tilt_long2))
+        
     #ステージ２
     if source['sma02'][last]>source['sma01'][last]>source['sma03'][last]:
         st.subheader("第2ステージ")
@@ -322,7 +334,7 @@ with col1:
             
             
 
-            last=99
+            last=999
             source['x'] = source["sma01"] - source["sma02"]
             source['y'] = source["sma03"] - source["sma01"]
             source['z'] = source["sma03"] - source["sma02"]
@@ -349,10 +361,25 @@ with col1:
                     source["stage"][a] = 6
                     stage6_list.append(a)
 
+            stage_now = source['stage'][last]
+            #st.table(source)
+            
+
+
+            for num in range(last+1):
+                index_num = last - num
+                if stage_now == source['stage'][index_num]:
+                    continue
+                if stage_now != source['stage'][index_num]:
+                    changed_when = index_num
+                    break
+
             
             stage_now = source['stage'][last]
             if stage_now == 5:
-                st.write(code)
+                st.subheader(code)
+                st.write(str(source['Date'][changed_when])+'からこのステージ')
+                
 
 
 with col2:
@@ -390,7 +417,7 @@ with col2:
             
             
 
-            last=99
+            last=999
             source['x'] = source["sma01"] - source["sma02"]
             source['y'] = source["sma03"] - source["sma01"]
             source['z'] = source["sma03"] - source["sma02"]
@@ -417,10 +444,24 @@ with col2:
                     source["stage"][a] = 6
                     stage6_list.append(a)
 
+            stage_now = source['stage'][last]
+            #st.table(source)
+            
+
+
+            for num in range(last+1):
+                index_num = last - num
+                if stage_now == source['stage'][index_num]:
+                    continue
+                if stage_now != source['stage'][index_num]:
+                    changed_when = index_num
+                    break
+
             
             stage_now = source['stage'][last]
             if stage_now == 6:
-                st.write(code)
+                st.subheader(code)
+                st.write(str(source['Date'][changed_when])+'からこのステージ')
 with col3:
     if st.button('ステージ1を探す'):
         for code in codes1:
@@ -456,7 +497,7 @@ with col3:
             
             
 
-            last=99
+            last=999
             source['x'] = source["sma01"] - source["sma02"]
             source['y'] = source["sma03"] - source["sma01"]
             source['z'] = source["sma03"] - source["sma02"]
@@ -483,11 +524,28 @@ with col3:
                     source["stage"][a] = 6
                     stage6_list.append(a)
 
+            stage_now = source['stage'][last]
+            #st.table(source)
+            
+
+
+            for num in range(last+1):
+                index_num = last - num
+                if stage_now == source['stage'][index_num]:
+                    continue
+                if stage_now != source['stage'][index_num]:
+                    changed_when = index_num
+                    break
+
             
             stage_now = source['stage'][last]
             if stage_now == 1:
-                st.write(code)
+                st.subheader(code)
+                st.write(str(source['Date'][changed_when])+'からこのステージ')
+
 
 
 
     
+
+
