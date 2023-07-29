ac7a90c0a6579bc8a737242cda04bbf28e627a53
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2023-07-29 10:19:04 +0900

    Create finance check.py

---
+++ pages/finance check.py
@@ -0,0 +1,115 @@
+from math import nan
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+import re
+import matplotlib
+import matplotlib.pyplot as plt
+import yfinance as yf
+import altair as alt
+import os
+import sys
+import subprocess
+
+def get_kessan(option):
+    # 指定URLのHTMLデータを取得
+    code = int(option)
+    url = "https://minkabu.jp/stock/{0:d}/settlement".format(code)
+    html = requests.get(url)
+    # BeautifulSoupのHTMLパーサーを生成
+    soup = BeautifulSoup(html.content, "html.parser")
+ 
+    # 全<table>要素を抽出
+    table_all = soup.find_all('table')
+ 
+    # 決算情報の<table>要素を検索する。
+    fin_table1 = None
+    for table in table_all:
+   
+        # <caption>要素を取得
+        caption = table.find('caption')
+        if caption is None:
+            continue
+   
+        # <caption>要素の文字列が目的のものと一致したら終了
+        if caption.text == '決算情報':
+            fin_table1 = table
+            break
+ 
+    # <table>要素内のヘッダ情報を取得する。
+    headers = []
+    thead_th = fin_table1.find('thead').find_all('th')
+    for th in thead_th:
+        headers.append(th.text)
+ 
+    # <table>要素内のデータを取得する。
+    rows = []
+    tbody_tr = fin_table1.find('tbody').find_all('tr')
+    for tr in tbody_tr:
+   
+        # 1行内のデータを格納するためのリスト
+        row = []
+   
+        # <tr>要素内の<th>要素を取得する。
+        th = tr.find('th')
+        row.append(th.text)
+   
+        # <tr>要素内の<td>要素を取得する。
+        td_all = tr.find_all('td')
+        for td in td_all:
+            row.append(td.text)
+   
+        # 1行のデータを格納したリストを、リストに格納
+        rows.append(row)
+ 
+    # DataFrameを生成する
+    df2 = pd.DataFrame(rows, columns=headers)
+ 
+    # 先頭の列(決算期)をインデックスに指定する
+    df2 = df2.set_index(headers[0])
+    return(df2)
+
+# 数値のカンマを削除する関数
+def trim_camma_kessan(x):
+    # 2,946,639.3のようなカンマ区切り、小数点有りの数値か否か確認する
+    comma_re = re.search(r"(\d{1,3}(,\d{3})*(\.\d+){0,1})", x)
+    if comma_re:
+        value = comma_re.group(1)
+        value = value.replace(',', '') # カンマを削除
+        return value
+   
+    return x
+ 
+codes = [1332, 1376, 1379, 1380]
+if st.button('計算を行う'):
+    for code in codes1_3:
+        option = code
+        ticker = str(option) + '.T'
+        tkr = yf.Ticker(ticker)
+        hist = tkr.history(period='1000d')
+        hist = hist.reset_index()
+        hist = hist.set_index(['Date'])
+        hist = hist.rename_axis('Date').reset_index()
+        hist = hist.T
+        a = hist.to_dict()
+
+        for items in a.values():
+                time = items['Date']
+                items['Date'] = time.strftime("%Y/%m/%d")
+
+        b = [x for x in a.values()]
+
+        source = pd.DataFrame(b)
+      
+        df2 = pd.DataFrame(get_kessan(option))
+        st.subheader('<決算情報>')
+        st.table(df2)
+    
+        # 各列に対して、trim_cammaを適用する(決算)
+        new_df2 = df2.copy()
+        for col in df2.columns:
+            new_df2[col] = df2[col].map(lambda v : trim_camma_kessan(v))
+          
