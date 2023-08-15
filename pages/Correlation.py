6a02be88f124f13ce37984e9a5cfc4023644fbbb
brocklee786 <109401403+brocklee786@users.noreply.github.com>
2023-08-15 15:11:15 +0900

    Update Correlation.py

--- pages/Correlation.py
+++ pages/Correlation.py
@@ -7,6 +7,7 @@ import streamlit as st
 
 option1 = st.text_input('1つ目の銘柄コードを入力してください')
 option2 = st.text_input('２つ目の銘柄コードを入力してください')
+st.title('相関チェック')
 if option1 and option2:
     #一つ目のデータ
     ticker = str(option1) + '.T'
@@ -27,6 +28,8 @@ if option1 and option2:
     source1 = pd.DataFrame(b)
     Date1 = source1['Date']
     stock_data1 = source1['Close']
+    stock_data1["PriceDifference"] = (stock_data1["Close"].pct_change()) * 100
+    price_change1 = stock_data1["PriceDifference"]
     #二つ目のデータ
     ticker = str(option2) + '.T'
     tkr = yf.Ticker(ticker)
@@ -46,15 +49,17 @@ if option1 and option2:
     source2 = pd.DataFrame(b)
     Date2 = source2['Date']
     stock_data2 = source2['Close']
+    stock_data2["PriceDifference"] = (stock_data2["Close"].pct_change()) * 100
+    price_change2 = stock_data2["PriceDifference"]
 
     code1 = option1
     code2 = option2
 
-    correlation = np.corrcoef(stock_data1, stock_data2)[1, 0]
+    correlation = np.corrcoef(price_change1, price_change2)[1, 0]
 
     fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(Date1.values, stock_data1.values, label=f" {code1}")
-    ax.plot(Date2.values, stock_data2.values, label=f" {code2}")
+    ax.plot(Date1.values, price_change1.values, label=f" {code1}")
+    ax.plot(Date2.values, price_change2.values, label=f" {code2}")
     ax.xaxis.set_major_locator(mdates.MonthLocator(interval=5))
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
     plt.xlabel("Date")
